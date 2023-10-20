4f57552c3fcaebd4f17fd727fdd78480df02cea2
Andrey Shyshko <andrey.shyshko84@gmail.com>
2023-10-20 13:02:50 +0300

    create slider

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".swiper-carusel", {
+    speed: 400,
+    slidesPerView: 1.6,
+    spaceBetween: 0,
+    loop: true,
+    centeredSlides: true,
+    grabCursor: true,
+  });
