a53780f2203028123de143f6d9eb06a66be79ea7
Andrey Shyshko <andrey.shyshko84@gmail.com>
2023-10-20 16:40:35 +0300

    add media query

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 var swiper = new Swiper(".swiper-carusel", {
     speed: 400,
-    slidesPerView: 1.6,
+    slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
     centeredSlides: true,
     grabCursor: true,
+    breakpoints: {
+      767: {
+        slidesPerView: 1.6,
+      },
+    },
   });
